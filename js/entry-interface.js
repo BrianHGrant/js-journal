a3ea654b4580fd92d0275438299bdee6fd4253c8
Brian Grant and Joel Delight <brian@>
2016-08-01 11:33:56 -0700

    create user entry interface

---
+++ js/entry-interface.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#journal-entry-form').submit(function(event) {
+    event.preventDefault();
+    var userEntry = $('#entry').val();
+    var currentEntry = new Entry(userEntry);
+    var wordCount = splitWords(userEntry).length;
+    var vowelCount = currentEntry.countVowels();
+    var consanantCount = currentEntry.countConsanants();
+    var currentTeaser = currentEntry.teaser();
+    $('#entries').append("<p>" + currentTeaser + "</p>")
+    $('#entries').append("<p>" + wordCount + "</p>")
+    $('#entries').append("<p>" + vowelCount + "</p>")
+    $('#entries').append("<p>" + consanantCount + "</p>")
+  });
+});
